74e546be515f51d2849b5315b79bbadf1727ba54
Ian <latourian@hotmail.com>
2020-04-25 11:36:33 -0300

    113625052020_master

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,12 @@
 const functions = require("firebase-functions");
 const app = require("express")();
 
-const { getAllTodos } = require("./APIs/todos");
+const { getAllTodos, postOneTodo, deleteTodo } = require("./APIs/todos");
+const { loginUser, signUpUser } = require("./APIs/users");
 
 app.get("/todos", getAllTodos);
+app.post("/todo", postOneTodo);
+app.post("/login", loginUser);
+app.delete("/todo/:todoId", deleteTodo);
+app.post("/signup", signUpUser);
 exports.api = functions.https.onRequest(app);
